687874717ee582c8332d64d8f2d505c51ebb1d8a
AyOhEe <74308087+AyOhEe@users.noreply.github.com>
2023-02-13 14:04:37 +0000

    Neuron state is now graphed in the example

--- Neuron.py
+++ Neuron.py
@@ -2,8 +2,6 @@
 
 """
 
-import numpy as np
-
 
 class Neuron:
     #class variables. mostly just settings related to global neuron function
@@ -13,30 +11,41 @@ class Neuron:
 
     #TODO base these off of literature. default values should assume 1ms/timestep 
     #(SUBJECT TO CHANGE, ALSO BASED ON LITERATURE. PROVIDE SOURCES!)
+    #TODO some factors could be provided as optional parameters in the constructor
+    #(i.e. excitation_decay)
+
+    #NOTE it looks like when this is actually being used, most coefficients will have to be
+    #dialed way down, including the timestep duration. would probably need to tick these once
+    #per millisecond for responsive behaviour. that isn't too bad though; it encourages
+    #longer neural circuits. it'll be especially interesting to see these interact with a
+    #continuous environment.
 
     #the value that excitation must surpass for a spike to occur
     spike_threshold = 100
     #the value that charge is set to when a spike occurs
-    spike_charge = 3
+    spike_charge = 1
     #the neurotransmitter cost coefficient while the neuron is spiking
-    spike_cost_coeff = 5
+    spike_cost_coeff = 15
+
+    #TODO make refractory period proportional to the % neurotransmitters emitted,
+    #which should emphasise synaptic fatigue
 
     #the value that refractory_state is set to when a spike occurs
-    refractory_period = 7
+    refractory_period = 5
 
     #the baseline neurotransmitter level
     available_neurotrans = 100
     #the neurotransmitter reuptake coefficient. controls how quickly reuptake occurs. [0, 1]
-    reuptake_coeff =  0.1
+    reuptake_coeff =  0.008
     #the amount of neurotransmitters required to start a spike
-    neurotrans_threshold = 30
+    neurotrans_threshold = 40
 
     #the factor by which excitation decays by each timestep. irrespective of any other factor. 
-    #multiplicative
-    excitation_decay = 0.99
+    #additive
+    excitation_decay = 1
     #the amount that refractory_state decays by each timestep. irrespective of any other factors. 
     #additive
-    refractory_decay = 1
+    refractory_decay = 0.5
     #the amount that charge decays by each timestep. irrespective of any other factors. additive
     charge_decay = 1
 
@@ -53,7 +62,7 @@ class Neuron:
     #swi: the sum of the neuron's weighted inputs
     def step(self, swi):
         #update excitation
-        self.excitation *= Neuron.excitation_decay
+        self.excitation -= Neuron.excitation_decay
         self.excitation += swi
 
         #are we spiking?
@@ -103,18 +112,96 @@ class Neuron:
         return txt
 
 if __name__ == "__main__":
-    import time
     import random
+    import matplotlib.animation as animation
+    import matplotlib.pyplot as plt
+    import numpy as np
 
     #test settings
-    n_ticks = 500
-    tick_delay_ms = 60
+    n_ticks = 2000
+    tick_delay_ms = 20
+
+    period = 400
+    sin_offset = 0.75
+    neuron_input = 20
 
 
-    #create a neuron, and provide it with constant input for n_ticks iterations
+    #TODO i'd like these graphs to be slightly prettier, with some lines showing thresholds
+    #and default values on things like excitation and neurotransmitters
+    
+    #set up the matplot plots
+    fig, axes = plt.subplots(5, 1)
+    fig.set_figheight(8)
+    fig.set_figwidth(15)
+
+    axes[0].set_xlim(0, n_ticks)
+    axes[0].set_ylim(0, 150)
+    axes[0].set_ylabel("Excitation")
+    excitation_line, = axes[0].plot([], [], lw=2)
+    
+    axes[1].set_xlim(0, n_ticks)
+    axes[1].set_ylim(0, Neuron.spike_charge*1.1)
+    axes[1].set_ylabel("Charge")
+    charges_line, = axes[1].plot([], [], lw=2)
+    
+    axes[2].set_xlim(0, n_ticks)
+    axes[2].set_ylim(0, Neuron.refractory_period*1.1)
+    axes[2].set_ylabel("Refractory state")
+    refractor_line, = axes[2].plot([], [], lw=2)
+    
+    axes[3].set_xlim(0, n_ticks)
+    axes[3].set_ylim(0, Neuron.available_neurotrans*1.1)
+    axes[3].set_ylabel("Neurotransmitters")
+    neurotrans_line, = axes[3].plot([], [], lw=2)
+    
+    axes[4].set_xlim(0, n_ticks)
+    axes[4].set_ylim(0, 1.1)
+    axes[4].set_ylabel("Output")
+    axes[4].set_xlabel("Time")
+    output_line, = axes[4].plot([], [], lw=2)
+
+    #data logging
+    times = []
+    excitations = []
+    charges = []
+    refractor_states = []
+    neurotrans = []
+    outputs = []
+
+    #set up the tick function
     neuron = Neuron()
 
-    for t in range(n_ticks):
-        input = 8 * random.random()
+    def do_tick(t):
+        #do a neuron timestep
+        input = neuron_input * random.random() * max(min(np.sin(t*np.pi*2 / period)+sin_offset, 1), 0)
         print(neuron.step(input), neuron, input)
-        time.sleep(tick_delay_ms / 1000)
+
+        #store the neuron's internal state and output
+        times.append(t)
+        excitations.append(neuron.excitation)
+        charges.append(neuron.charge)
+        refractor_states.append(neuron.refractory_state)
+        neurotrans.append(neuron.neurotrans)
+        outputs.append(neuron.output())
+
+        #update the lines
+        excitation_line.set_data(times, excitations)
+        charges_line.set_data(times, charges)
+        refractor_line.set_data(times, refractor_states)
+        neurotrans_line.set_data(times, neurotrans)
+        output_line.set_data(times, outputs)
+
+        return excitation_line, charges_line, refractor_line, neurotrans_line, output_line,
+        
+
+
+    def init():
+        return excitation_line, charges_line, refractor_line, neurotrans_line, output_line,
+
+    anim = animation.FuncAnimation(fig, do_tick,
+                            init_func=init,
+                            frames = n_ticks,
+                            interval = tick_delay_ms,
+                            blit = True,
+                            repeat = False)
+    plt.show()
